10a34ab89e19dbda5df6220c593b0bb54344ffc6
Florian Thiel <flth@flth.de>
2026-06-02 00:01:08 +0200

    MUs hinzugefügt

---
+++ vehicle/waggon/autotransportwagen/westerlandeinheit_alt.mu.lua
@@ -0,0 +1,21 @@
+function data()
+return {
+  vehicles = {
+    { name = "offhs66_dbbraun_einheit_front.mdl", forward = false },
+	{ name = "laes549_anfang.mdl", forward = false },
+    { name = "laes549_mitte.mdl", forward = true },
+	{ name = "laes549_mitte.mdl", forward = true },
+	{ name = "laes549_mitte.mdl", forward = true },
+	{ name = "laes549_mitte.mdl", forward = true },
+	{ name = "laes549_mitte.mdl", forward = true },
+	{ name = "laes549_mitte.mdl", forward = true },
+	{ name = "laes549_mitte.mdl", forward = true },
+	{ name = "laes549_ende.mdl", forward = true },
+	{ name = "offhs66_dbbraun_einheit.mdl", forward = true },
+  },
+  name = _("Westerlandeinheit"),
+  desc = _(""),
+  groupFileName = "menu_autotransport_person.mdl",
+
+}
+end
---
+++ vehicle/waggon/autotransportwagen/westerlandeinheit_blau.mu.lua
@@ -0,0 +1,22 @@
+function data()
+return {
+  vehicles = {
+    { name = "leks554_dbblau_einheit_front.mdl", forward = false },
+	{ name = "laaeks555_anfangfront.mdl", forward = false },
+    { name = "laaeks555_anfangschrag.mdl", forward = false },
+	{ name = "laaeks555_mitte.mdl", forward = false },
+	{ name = "laaeks555_mitte.mdl", forward = false },
+	{ name = "laaeks555_mitte.mdl", forward = false },
+	{ name = "laaeks555_mitte.mdl", forward = false },
+	{ name = "laaeks555_mitte.mdl", forward = false },
+	{ name = "laaeks555_mitte.mdl", forward = false },
+	{ name = "laaeks555_endeschrag.mdl", forward = true },
+	{ name = "laaeks555_endefront.mdl", forward = true },
+	{ name = "leks554_dbblau_einheit.mdl", forward = true },
+  },
+  name = _("Westerlandeinheit"),
+  desc = _(""),
+   groupFileName = "menu_autotransport_person.mdl",
+
+}
+end
